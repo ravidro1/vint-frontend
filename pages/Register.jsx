23f1d41ec00ed4672249831b9b20328be8355008
talbenari <talbenari865@gmail.com>
2023-03-01 13:56:16 +0200

    register page and its files

---
+++ pages/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from '../components/Login';
+
+function Register() {
+
+
+
+    return (
+        <View>
+            <Login />
+        </View>
+    )
+}
+
+export default Register
