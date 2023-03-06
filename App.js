e987ad42cc599d75e84c07db5f6740bc09ca0af7
talbenari <talbenari865@gmail.com>
2023-03-06 10:38:11 +0200

    profile page

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Landing from "./screens/Landing";
+import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-
+import Profile from './pages/profile';
 import Tabs from "./navigation/tabs";
 import Post from './components/Post';
 
-const App = () => {
+function App() {
   return (
     <NavigationContainer>
       <View className='flex-1'>
+        <StatusBar style="auto" />
         <Tabs />
       </View>
     </NavigationContainer>
